ab2408cceea44660dd7d2f8dc3d96b127038f64c
ivladek <vladek@me.com>
2023-09-04 16:16:41 +0300

    04.09

---
+++ ws2019-prepara.ps1
@@ -0,0 +1,3 @@
+
+# Get Python from https://www.python.org/downloads/windows
+Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.11.4/python-3.11.4-amd64.exe" -OutFile "%USERPROFILE\Downloads\python-installer.exe"
